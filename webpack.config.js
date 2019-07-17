d24b3253735bfa8991a9e8c16bf4947ba5fe866f
Erick Bello <erick.bonas@gmail.com>
2019-07-17 14:58:43 -0300

    chore: setting do webpack

---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+
+function generateConfig() {
+	const babelLoader = {
+		loader: 'babel-loader',
+		options: {
+			presets: ['@babel/preset-env'],
+			plugins: [],
+		},
+	};
+
+	return {
+		entry: {
+			'layout-buefy-base': './src/buefy-base/index.js',
+		},
+		output: {
+			path: path.resolve(__dirname, 'dist'),
+			filename: '[name].js',
+			libraryTarget: 'commonjs2',
+		},
+		resolve: {
+			extensions: ['.tsx', '.ts', '.jsx', '.js', '.json'],
+			alias: {
+				'~': path.resolve(__dirname, 'src'),
+			},
+		},
+		module: {
+			rules: [
+				{
+					test: /\.m?js$/,
+					exclude: /(node_modules|bower_components)/,
+					use: babelLoader,
+				},
+				{
+					test: /\.vue$/,
+					loader: 'vue-loader',
+					options: {
+						optimizeSSR: false,
+					},
+				},
+				{
+					test: /\.tsx?$/,
+					exclude: /node_modules/,
+					use: [
+						babelLoader,
+						{
+							loader: 'ts-loader',
+							options: {
+								appendTsSuffixTo: [/\.vue$/],
+							},
+						},
+					],
+				},
+				{
+					test: /\.pug?$/,
+					use: 'pug-plain-loader',
+					exclude: /node_modules/,
+				},
+				{
+					test: /\.scss$/,
+					use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+				},
+			],
+		},
+		plugins: [
+			new VueLoaderPlugin(),
+			new MiniCssExtractPlugin({
+				filename: '[name].min.css',
+			}),
+		],
+		externals: {
+		},
+	};
+};
+
+module.exports = generateConfig();
